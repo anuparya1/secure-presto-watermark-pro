0b5a36f2e46aadd0520c93653a1646436963b0e2
anuparya1 <anupkumarscesupaul@gmail.com>
2026-08-01 18:52:05 +0530

    Add plugin loader
    Added a class Loader for plugin initialization.

--- includes/class-loader.php
+++ includes/class-loader.php
@@ -1,1 +1,33 @@
+<?php
+/**
+ * Plugin Loader
+ *
+ * @package SecurePrestoWatermarkPro
+ */
 
+declare(strict_types=1);
+
+namespace SPWP;
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+final class Loader {
+
+	/**
+	 * Plugin boot.
+	 *
+	 * @return void
+	 */
+	public static function init(): void {
+
+		// Future:
+		// Admin
+		// Settings
+		// Watermark
+		// LearnPress
+		// Presto Player
+	}
+
+}
